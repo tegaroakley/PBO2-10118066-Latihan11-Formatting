08bdee48cc2f2a8a820857f4ef6e7471d5adfeba
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 16:00:57 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan11/formatting/PBO210118066Latihan11Formatting.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan11.formatting;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menunjukkan sistem Formatting
+ */
+public class PBO210118066Latihan11Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
